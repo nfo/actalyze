6e8179bce646b1d07771ae9d9b7eea510ddbe980
Nicolas Fouché <nicolas.fouche@gmail.com>
2012-05-12 11:54:22 +0200

    Make the cookie expire 5 years from now. Without setting an expiration date, the cookie is deleted when the browser closes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,9 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  # This UUID will be added to the episodejs beacon params
   def set_uuid_cookie
-    cookies[:uuid] ||= UUIDTools::UUID.random_create.to_s
+    uuid = cookies[:uuid] || UUIDTools::UUID.random_create.to_s
+    cookies[:uuid] = { value: uuid, expires: 5.years.from_now }
   end
 end
