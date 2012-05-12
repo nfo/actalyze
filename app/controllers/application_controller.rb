3889b62f71d218a11870d9871130555c1955de06
Nicolas Fouché <nicolas.fouche@gmail.com>
2012-05-12 11:37:06 +0200

    Set the tracker UUID cookie if not already set.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
+require 'uuid_tools/uuid'
+
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_uuid_cookie
+
+  protected
+
+  def set_uuid_cookie
+    cookies[:uuid] ||= UUIDTools::UUID.random_create.to_s
+  end
 end
